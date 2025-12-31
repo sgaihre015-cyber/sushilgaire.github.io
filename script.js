a8ed4441369968a275f07574573eab5e6311cd10
sushil-123 <sgaihre015@gmail.com>
2025-12-31 10:44:46 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
